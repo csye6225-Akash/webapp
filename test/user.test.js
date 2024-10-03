93415188d06cbc0776fb17847d5bb0026efd4484
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-03 03:56:20 -0400

    Added Unit Test cases using ChaiMocha

---
+++ test/user.test.js
@@ -0,0 +1,59 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app.js'); 
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('User API Tests', () => {
+    let userId;
+
+    before((done) => {
+        
+        done();
+    });
+
+    it('should create a new user successfully', (done) => {
+        chai.request(app)
+            .post('/v1/user')
+            .send({
+                email: 'tes456t1@exmple.com',
+                password: 'Password@123',
+                first_name: 'Test',
+                last_name: 'User'
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(201);
+                userId = res.body.id; 
+            });
+    });
+
+    it('should not create a user with an invalid email', (done) => {
+        chai.request(app)
+            .post('/v1/user')
+            .send({
+                email: 'invalid-email',
+                password: 'Password@123',
+                first_name: 'Test',
+                last_name: 'User'
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
+
+    it('should not create a user with missing fields', (done) => {
+        chai.request(app)
+            .post('/v1/user')
+            .send({
+                password: 'Password@123'
+            })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
+
+    
+});
