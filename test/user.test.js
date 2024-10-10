4d07abfc40fd8d30be4da74a87cd741c9f40c6cc
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-10 13:00:14 -0400

    Action Testing

--- test/user.test.js
+++ test/user.test.js
@@ -14,23 +14,6 @@ describe('User API Tests', () => {
       
       done();
     });
-  
-    it('should create a new user successfully', (done) => {
-      chai.request(app)
-        .post('/v1/user')
-        .send({
-          email: 's456t1@exmple.com',
-          password: 'Password@123',
-          first_name: 'Test',
-          last_name: 'User'
-        })
-        .end((err, res) => {
-          if (err) done(err); 
-          expect(res).to.have.status(201);
-          userId = res.body.id; 
-          done(); // test will end here
-        });
-    });
 
     it('should not create a user with an invalid email', (done) => {
         chai.request(app)
