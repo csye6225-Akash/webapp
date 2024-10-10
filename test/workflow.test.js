27e56ee1c26b8074e342c413d453e660f413c3ba
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-10 03:22:29 -0400

    using secret (#23)
    * using secret
    
    * using secret
    
    * using secret
    
    * using secret
    
    * using secret
    
    * using secret
    
    * using secret

--- test/workflow.test.js
+++ test/workflow.test.js
@@ -21,31 +21,3 @@ describe('/healthz endpoint', function() {
   }).timeout(2000);
 });
 
-// describe('POST /v1/user', function() {
-//     it('should not create a user with an invalid email', function(done) {
-//       chai.request(app)
-//         .post('/v1/user')
-//         .send({
-//           email: 'invalid-email',
-//           password: 'Password@123',
-//           first_name: 'Test',
-//           last_name: 'User'
-//         })
-//         .end(function(err, res) {
-//           expect(res).to.have.status(400);
-//           done();
-//         });
-//     }).timeout(2000);
-  
-//     it('should not create a user with missing fields', function(done) {
-//       chai.request(app)
-//         .post('/v1/user')
-//         .send({
-//           password: 'Password@123'
-//         })
-//         .end(function(err, res) {
-//           expect(res).to.have.status(400);
-//           done();
-//         });
-//     }).timeout(2000); 
-//   });
