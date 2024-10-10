8d3faeada18712271c3930b338afbee619d9a448
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-10 01:27:30 -0400

    test5

--- test/workflow.test.js
+++ test/workflow.test.js
@@ -22,30 +22,30 @@ describe('/healthz endpoint', function() {
 });
 
 describe('POST /v1/user', function() {
-  it('should not create a user with an invalid email', function(done) {
-    chai.request(app)
-      .post('/v1/user')
-      .send({
-        email: 'invalid-email',
-        password: 'Password@123',
-        first_name: 'Test',
-        last_name: 'User'
-      })
-      .end(function(err, res) {
-        expect(res).to.have.status(400);
-        done();
-      });
+    it('should not create a user with an invalid email', function(done) {
+      chai.request(app)
+        .post('/v1/user')
+        .send({
+          email: 'invalid-email',
+          password: 'Password@123',
+          first_name: 'Test',
+          last_name: 'User'
+        })
+        .end(function(err, res) {
+          expect(res).to.have.status(400);
+          done();
+        });
+    }).timeout(2000);
+  
+    it('should not create a user with missing fields', function(done) {
+      chai.request(app)
+        .post('/v1/user')
+        .send({
+          password: 'Password@123'
+        })
+        .end(function(err, res) {
+          expect(res).to.have.status(400);
+          done();
+        });
+    }).timeout(2000); 
   });
-
-  it('should not create a user with missing fields', function(done) {
-    chai.request(app)
-      .post('/v1/user')
-      .send({
-        password: 'Password@123'
-      })
-      .end(function(err, res) {
-        expect(res).to.have.status(400);
-        done();
-      });
-  }).timeout(2000)
-});
