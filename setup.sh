d498283f492456bc1df2dd4361aad5ce087392b4
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-17 03:03:06 -0400

    changes in yml

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Update the package list
@@ -8,19 +7,19 @@ sudo apt-get upgrade -y
 # Install Unzip
 sudo apt-get install -y unzip
 
-# Install Node.js and check the version
+# Install Node.js and NPM
 sudo apt-get install -y nodejs npm
 
-# Install NPM
-sudo apt-get install npm -y
-
-#Install MySQL server
+# Install MySQL server
 sudo apt-get install mysql-server -y
 
-# # Start the MySQL service and enable it
+# Start the MySQL service and enable it
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
+# Alter the MySQL root user password and create a database
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'akash'; FLUSH PRIVILEGES; CREATE DATABASE database_development;"
+
+npm install
 
 
- sudo mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'akash';FLUSH PRIVILEGES;CREATE DATABASE database_development;"
