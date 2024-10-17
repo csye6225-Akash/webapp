bf83d1a75f5e52be752aa107164638c4b804976b
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-16 20:53:53 -0400

    Workflow (#29)
    * Added a workflow
    
    * Added a workflow
    
    * Added a workflow
    
    * Added a workflow
    
    * Added a workflow
    
    * Added a workflow
    
    * Workflow

--- setup.sh
+++ setup.sh
@@ -1,3 +1,27 @@
-sudo apt update
 
-sudo apt upgrade -y
+#!/bin/bash
+
+# Update the package list
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install Unzip
+sudo apt-get install -y unzip
+
+# Install Node.js and check the version
+sudo apt-get install -y nodejs npm
+
+# Install NPM
+sudo apt-get install npm -y
+
+#Install MariaDB server
+sudo apt-get install mysql-server -y
+
+# # Start the MariaDB service and enable it
+sudo systemctl start mysql
+sudo systemctl enable mysql
+
+
+sudo mysql_secure_installation -y
+
+ sudo mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'akash';FLUSH PRIVILEGES;CREATE DATABASE database_development;"
